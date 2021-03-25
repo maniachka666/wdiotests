9ed7ed3e235040b3adc580f6599f8f2db29e2c93
IrynaKlymovych <iryna.klymovych97@gmail.com>
2021-03-25 12:58:15 -0700

    changes

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -5,6 +5,11 @@ class LoginPage extends Page {
     get inputUsername () { return $('#normal_login_email'); }
     get inputPassword () { return $('#normal_login_password'); }
     get buttonSubmit () { return $('.login-form-button'); }
+    get errorToast () { return $('.ant-notification-notice-message'); }
+
+    open () {
+        return super.open('/');
+    }
 
     setLogin (email) {
           this.inputUsername.setValue(email);
@@ -18,9 +23,14 @@ class LoginPage extends Page {
         this.buttonSubmit.click();
     }
 
-    open () {
-        return super.open('/');
+    submitButtonIsDisabled() {
+        expect(this.buttonSubmit).toBeDisabled();
     }
+
+    errorToastAppeared() {
+        expect(this.errorToast).toBeDisplayed();
+    }
+
 }
 
 export default new LoginPage();
